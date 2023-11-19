86b0408d92d7239f5a33025f255f4520f9d3fb71
az4r <113637712+az4r@users.noreply.github.com>
2023-11-19 17:12:10 +0100

    WORKING

---
+++ entry_button.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+def aktualizacja_pola2(*args):
+        entry2.delete(0, tk.END)
+        entry2.insert(0, float(entry1.get()) * 2)
+
+root = tk.Tk()
+root.title("ILOCZYN")
+
+entry1 = tk.Entry(root)
+entry1.grid(row=0, column=1)
+
+label1 = tk.Label(root, text=" x 2 = ")
+label1.grid(row=0, column=2)
+
+entry2 = tk.Entry(root)
+entry2.grid(row=0, column=3)
+
+calculate_button = tk.Button(root, text="Oblicz", command=aktualizacja_pola2)
+calculate_button.grid(row=1, column=2)
+
+root.mainloop()
